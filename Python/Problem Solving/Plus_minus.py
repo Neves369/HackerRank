72a374ae296f97ef6d86535f83de41e025703946
Neves369 <douglasbrian369@hotmail.com>
2023-08-10 10:53:42 -0300

    first commit

---
+++ Python/Problem Solving/Plus_minus.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    positive = 0
+    negative = 0
+    zero = 0
+
+    for i in arr:
+        if(i > 0):
+            positive += 1
+        elif(i < 0):
+            negative += 1
+        else:
+            zero += 1
+    
+    positive = round(positive/len(arr), 6)
+    negative = round(negative/len(arr), 6)
+    zero = round(zero/len(arr), 6)
+
+    return [positive, negative, zero]
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = plusMinus(arr)
+    
+    print("{:.6f}".format(result[0]))
+    print("{:.6f}".format(result[1]))
+    print("{:.6f}".format(result[2]))
+    
